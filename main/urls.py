ae16eec69dad5a21c18e1952ff57ab9901867499
Isidore DelPierro <fontadore@gmail.com>
2022-02-19 07:54:17 +0100

    Lesson One: 1) Routing on urls.py from the primary app 2) Create & Route on urls.py in our app 3) New view called "homepage"

---
+++ main/urls.py
@@ -0,0 +1,19 @@
+"""mysite URL Configuration
+
+        This is our app called main
+
+        app_name = "main" is useful
+        when you are trying to create custom urls
+        that will point to a specific view,
+        but you don't have to hard code the urls
+
+"""
+from django.urls import path
+from . import views
+
+app_name = "main"
+
+urlpatterns = [
+    path("", views.homepage, name="homepage"),
+
+]
