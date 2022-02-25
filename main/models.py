5e3fb3c3c68122f6b2b3ae4cd13aba5d1de52368
Isidore DelPierro <fontadore@gmail.com>
2022-02-25 07:38:31 +0100

    Lesson Two: Models
    1) Switched sqlite to MySQL
    
    2) Tutorial model created

--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+#cf django model fields on djangoproject.com
+
+class Tutorial(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published = models.DateTimeField("date published")
+
+    def __str__(self):
+        return str(self.title) 
