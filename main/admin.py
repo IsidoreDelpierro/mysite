f52dbaacc162c61786c7cdd5fcd87d286acb8949
Isidore DelPierro <fontadore@gmail.com>
2022-02-25 09:00:19 +0100

    Lesson Three: Admin & Apps
    1) Create superuser 
    
    2) Register Tutorial on admin + config
    
    3) Install + register + config TinyMCE

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Tutorial
+from tinymce.widgets import TinyMCE
+from django.db import models
 
 # Register your models here.
+
+class TutorialAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Title/Date", {"fields": ["title", "published"]}),
+        ("Content", {"fields": ["content"]})
+    ]
+
+    #override the text field in Tutorial model so it uses TinyMCE editor
+    formfield_overrides = {
+        models.TextField: {'widget': TinyMCE()}
+    }
+
+admin.site.register(Tutorial, TutorialAdmin)
--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 #cf django model fields on djangoproject.com
@@ -6,7 +7,7 @@ from django.db import models
 class Tutorial(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    published = models.DateTimeField("date published")
+    published = models.DateTimeField("date published", default=datetime.now())
 
     def __str__(self):
-        return str(self.title) 
+        return str(self.title)
