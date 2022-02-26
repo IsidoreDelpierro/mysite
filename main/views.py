8f961d13cc7c814773d49500a7bd6ed37adc1adc
Isidore DelPierro <fontadore@gmail.com>
2022-02-26 10:10:53 +0100

    Lesson Ten: Working With Foreign Keys
    1) Loop through Categories on home page, Series on sub pages, and pick 
    out the first Tutorial in each series.
    
    2) Single_Slug view, matching series, part ones 
    
    
    NB: Problem with Advanced Category: no matching query

--- main/views.py
+++ main/views.py
@@ -1,17 +1,38 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Tutorial
+from .models import Tutorial, Series, Category
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from .forms import NewUserForm
 
 # Create your views here.
+def single_slug(request, single_slug):
+    categories = [c.slug for c in Category.objects.all()]
+    if single_slug in categories:
+        matching_series = Series.objects.filter(category__slug=single_slug)
+
+        series_urls = {}
+        for m in matching_series.all():
+            part_one = Tutorial.objects.filter(series__series=m.series).earliest("published")
+            series_urls[m] = part_one.slug
+
+        return render(request,
+                        "main/category.html",
+                        {"part_ones": series_urls})
+
+    tutorials = [t.slug for t in Tutorial.objects.all()]
+    if single_slug in tutorials:
+        return HttpResponse(f"{single_slug} is a Tutorial!!!")
+
+    return HttpResponse(f"{single_slug} does not correspond to anything.")
+
+
 def homepage(request):
     #return HttpResponse("Wow this is an <strong>awesome</strong> tutorial")
     return render(request=request,
-        template_name="main/home.html",
-        context={"tutorials": Tutorial.objects.all()})
+        template_name="main/categories.html",
+        context={"categories": Category.objects.all()})
 
 def register(request):
     if request.method == "POST":
@@ -48,7 +69,7 @@ def login_request(request):
         if form.is_valid():
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
-            #you have to explicitly specify, else you get an error 
+            #you have to explicitly specify, else you get an error
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
